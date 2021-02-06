7263bb7538b7ef530ac8d9ff1804b12fd062fc37
Susanta <inbox.susanta@gmail.com>
2021-02-06 13:12:50 +0530

    Preparing Our Files for Go Live (Part 1)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 
 const postCSSPlugins = [
@@ -9,22 +10,8 @@ const postCSSPlugins = [
   require('autoprefixer'),
 ];
 
-module.exports = {
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app'),
-  },
-  devServer: {
-    before: function (app, server) {
-      server._watch('./app/**/*.html');
-    },
-    contentBase: path.join(__dirname, 'app'),
-    hot: true,
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  mode: 'development',
   module: {
     rules: [
       {
@@ -41,3 +28,30 @@ module.exports = {
     ],
   },
 };
+
+if (currentTask == 'dev') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app'),
+  };
+  config.devServer = {
+    before: function (app, server) {
+      server._watch('./app/**/*.html');
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0',
+  };
+  config.mode = 'development';
+}
+
+if (currentTask == 'build') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'dist'),
+  };
+  config.mode = 'production';
+}
+
+module.exports = config;
